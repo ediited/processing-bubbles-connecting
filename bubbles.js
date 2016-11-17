1c4c8f417df08b60e94e96cdb5324aeb7fb95464
Lauritz Kramberger <lauritz.kramberger@gmail.com>
2016-11-17 15:50:55 +0100

    now colorfull & done

--- bubbles.js
+++ bubbles.js
@@ -1,4 +1,5 @@
-var anzahl = 1000;
+var anzahl = 500;
+noFill();
 var kugel = function(){
  this.x=0;
  this.y=0;
@@ -6,26 +7,17 @@ var kugel = function(){
  this.ydir=0;
 };
 var kugeln = [];
-/*
 for (var i = 0 ; i < anzahl ; i++ ){
     kugeln[i]=new kugel();
     kugeln[i].x= floor(random(-width*2,0));
     kugeln[i].y= floor(random(-height*2,0));
-    kugeln[i].xdir = round(random(1,3));
+    kugeln[i].xdir = round(random(1,5));
     kugeln[i].ydir = kugeln[i].xdir;
 
 }
-*/
 
 var draw = function() {
     background(255, 255, 255);
-    
-    append(kugeln,new kugel());
-    kugeln[kugeln.length-1].x= floor(random(-width*2,0));
-    kugeln[kugeln.length-1].y= floor(random(-height*2,0));
-    kugeln[kugeln.length].xdir = round(random(1,3));
-    kugeln[kugeln.length].ydir = kugeln[kugeln.length-1].xdir;
-    
     for (var i = 0; i < anzahl ; i++ ){
     kugeln[i].x = kugeln[i].x+kugeln[i].xdir;
     kugeln[i].y = kugeln[i].y+kugeln[i].ydir;
@@ -35,6 +27,8 @@ var draw = function() {
         kugeln[i].xdir = round(random(1,3));
         kugeln[i].ydir = kugeln[i].xdir;
         }
+   stroke(0, 200-kugeln[i].xdir*20, 255);
+   strokeWeight(kugeln[i].xdir);
     ellipse(kugeln[i].x,kugeln[i].y,kugeln[i].xdir*10,kugeln[i].xdir*10);
     }
 };
