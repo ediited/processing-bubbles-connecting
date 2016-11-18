dd57a0b83aca91409f89dec1101e0e6b3f7b4235
Lauritz Kramberger <lauritz.kramberger@gmail.com>
2016-11-18 16:19:41 +0100

    now with bouncing

--- bubbles.js
+++ bubbles.js
@@ -1,11 +1,19 @@
 var anzahl = 500;
-noFill();
+var abprall = false;
+frameRate(60);
+
 var kugel = function(){
  this.x=0;
  this.y=0;
  this.xdir=0;
  this.ydir=0;
 };
+
+var bubble = new kugel();
+bubble.xdir = 3;
+bubble.ydir = 3;
+
+
 var kugeln = [];
 for (var i = 0 ; i < anzahl ; i++ ){
     kugeln[i]=new kugel();
@@ -13,22 +21,58 @@ for (var i = 0 ; i < anzahl ; i++ ){
     kugeln[i].y= floor(random(-height*2,0));
     kugeln[i].xdir = round(random(1,5));
     kugeln[i].ydir = kugeln[i].xdir;
-
+    
 }
 
 var draw = function() {
     background(255, 255, 255);
     for (var i = 0; i < anzahl ; i++ ){
-    kugeln[i].x = kugeln[i].x+kugeln[i].xdir;
-    kugeln[i].y = kugeln[i].y+kugeln[i].ydir;
+    
+    if (!abprall){
         if (kugeln[i].x>width+10||kugeln[i].y>height+10){
         kugeln[i].x= floor(random(-width*2,0));
         kugeln[i].y= floor(random(-height*2,0));
         kugeln[i].xdir = round(random(1,3));
         kugeln[i].ydir = kugeln[i].xdir;
         }
-   stroke(0, 200-kugeln[i].xdir*20, 255);
-   strokeWeight(kugeln[i].xdir);
-    ellipse(kugeln[i].x,kugeln[i].y,kugeln[i].xdir*10,kugeln[i].xdir*10);
     }
+    else{
+        
+        
+    if (kugeln[i].x>width+5){
+      kugeln[i].xdir = -kugeln[i].xdir;  
+    }
+    if (kugeln[i].y>height+5){
+     kugeln[i].ydir = -kugeln[i].ydir;   
+    }
+    if (kugeln[i].x<-5){
+      kugeln[i].xdir=-kugeln[i].xdir;
+    }
+    if (kugeln[i].y<-5){
+      kugeln[i].ydir = -kugeln[i].ydir;    
+    }    
+        
+        
+        
+        
+    }
+    
+    
+    kugeln[i].x = kugeln[i].x+kugeln[i].xdir;
+    kugeln[i].y = kugeln[i].y+kugeln[i].ydir;
+   stroke(0, 200-sqrt(pow(kugeln[i].xdir*20,2)), 255);
+   fill(0, 200-sqrt(pow(kugeln[i].xdir*20,2)), 255,50);
+   //strokeWeight(kugeln[i].xdir);
+    ellipse(kugeln[i].x,kugeln[i].y,sqrt(pow(kugeln[i].xdir*10,2)),sqrt(pow(kugeln[i].xdir*10,2)));
+    }
+};
+
+var mousePressed = function(){
+  if  (abprall){
+      abprall = false;
+  }
+  else{
+      abprall = true;   
+  }
+    
 };
