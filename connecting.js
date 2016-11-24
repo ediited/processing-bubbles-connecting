aec0e61f3870ec5cdef2508b33cf38f258af5e97
Lauritz Kramberger <lauritz.kramberger@gmail.com>
2016-11-24 16:40:13 +0100

    small changes

--- connecting.js
+++ connecting.js
@@ -6,6 +6,9 @@ var gewirr = function(startx, starty) {
         this.y = random(echty - 100, echty + 100);
         this.px = this.x;
         this.py = this.y;
+        this.returnx=0;
+        this.returny=0;
+    
     };
     var anzahl = round(random(3,6));
     var arr = [];
@@ -17,9 +20,10 @@ var gewirr = function(startx, starty) {
     this.color = [0, 0, 0];
 
     //background(0, 0, 0);
-
+    var resetcounter = 100;
+    var returning = true;
     this.drawGew = function() {
-
+    
         stroke(this.color[0], this.color[1], this.color[2]);
         for (var i = 0; i < anzahl; i++) {
             for (var k = 0; k < anzahl; k++) {
@@ -27,29 +31,38 @@ var gewirr = function(startx, starty) {
             }
         }
 
-
+        
         for (var i = 0; i < anzahl; i++) {
 
             if (mouseIsPressed) {
                 if (mouseX > arr[i].x) {
-                    arr[i].x = arr[i].x + (mouseX - arr[i].x) / 10;
+                    arr[i].x = arr[i].x + (mouseX - arr[i].x) / 200;
                 }
                 if (mouseX < arr[i].x) {
-                    arr[i].x = arr[i].x - (arr[i].x - mouseX) / 10;
+                    arr[i].x = arr[i].x - (arr[i].x - mouseX) / 200;
                 }
                 if (mouseY > arr[i].y) {
-                    arr[i].y = arr[i].y + (mouseY - arr[i].y) / 10;
+                    arr[i].y = arr[i].y + (mouseY - arr[i].y) / 200;
                 }
                 if (mouseY < arr[i].y) {
-                    arr[i].y = arr[i].y - (arr[i].y - mouseY) / 10;
+                    arr[i].y = arr[i].y - (arr[i].y - mouseY) / 200;
                 }
             }
 
-
+            else if(returning){
+                if (round(arr[i].x)===round(arr[i].px)){
+                    returning = false;
+                }
+                arr[i].x+=arr[i].returnx;
+                arr[i].y+=arr[i].returny;
+                //resetcounter++;
+            }
+            
+            
             //rect(-10,-10,width*2,height*2);
-            arr[i].x += random(-0.5, 0.5);
-            arr[i].y += random(-0.5, 0.5);
-
+            arr[i].x += random(-2.5, 2.5);
+            arr[i].y += random(-2.5, 2.5);
+            
         }
 
 
@@ -57,26 +70,39 @@ var gewirr = function(startx, starty) {
 
     };
     
+    
     this.reset = function(){
-      for (var i = 0; i<anzahl;i++){
-      arr[i].x=arr[i].px;
-      arr[i].y=arr[i].py;
       
+      for (var i = 0; i<anzahl;i++){
+                if (arr[i].px > arr[i].x) {
+                   arr[i].returnx=-((arr[i].x-arr[i].px)/100);
+                }
+                if (arr[i].px < arr[i].x) {
+                    arr[i].returnx=-((arr[i].x-arr[i].px)/100);
+                }
+                if (arr[i].py > arr[i].y) {
+                    arr[i].returny=-((arr[i].y-arr[i].py)/100);
+                }
+                if (arr[i].py < arr[i].y) {
+                    arr[i].returny=-((arr[i].y-arr[i].py)/100);
+                }
       }
+      resetcounter = 0;
+      returning = true;
     };
 
 
 };
 
-fill(255, 255, 255, 30);
+fill(0, 0, 0, 30);
 var gewarr = [];
 var anzahl = 51;
 for (var i = 0; i < anzahl; i++) {
 
-    gewarr[i] = new gewirr(random(-width, width*2), random(-height, height*2));
-    gewarr[i].color = [random(100, 230), random(100, 230), random(100, 230)];
+    gewarr[i] = new gewirr(random(-width, width+width/2), random(-height, height+height/2));
+    gewarr[i].color = [random(0, 200), random(0, 0), random(200, 230)];
 }
-strokeWeight(1);
+strokeWeight(0.5);
 
 var draw = function() {
     for (var i = 0; i < anzahl; i++) {
@@ -92,6 +118,10 @@ var draw = function() {
 
 var mouseReleased = function(){
   for (var i =0; i< anzahl;i++){
+  
    gewarr[i].reset();   
+  
+  
   }
+  
 };
